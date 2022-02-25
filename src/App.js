20a59a210d0eeb8d8ff9a6cf9a4c2d0425ea05af
raghadhethnawi <raghadhethnawi@gmail.com>
2022-02-25 15:51:15 +0200

    FromCreation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from './components/Form';
+
 class App extends Component {
+
+  getRecipe = (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    console.log(recipeName);
+    e.target.elements.recipeName.value = '';
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
       </div>
     );
   }
